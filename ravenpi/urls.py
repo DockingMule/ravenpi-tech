3dc19f509f8fd83862a09ae5c9cf8144dd032b67
DockingMule <aca0824@gmail.com>
2021-03-27 21:17:08 -0400

    did things

--- ravenpi/urls.py
+++ ravenpi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from raven import views
+from . import settings
 
 urlpatterns = [
     path('admin2/', admin.site.urls),
